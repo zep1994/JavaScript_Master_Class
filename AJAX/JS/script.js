2b82e2545ac7d5c8b133cd1ee4f481033f9e1d95
zep1994 <snaxbags@protonmail.com>
2020-06-17 21:45:25 -0500

    solve uppercase algo

--- AJAX/JS/script.js
+++ AJAX/JS/script.js
@@ -1,10 +1,12 @@
-$(document).ready(function() {
-    $.ajax({
-        url: 'https://api.nasa.gov/planetary/apod?api_key=lEuG08FTmJLg17cvemAzUrErh4gulvFkvipkG6H4',
-        data: data,
-        dataType: 'json',
-        success: function(data) {
-            console.log(data)
-        },
+$(document).ready(function() {  
+    $("#submit").on('click', function() {
+        $.ajax({
+            url: 'https://api.nasa.gov/planetary/apod?api_key=lEuG08FTmJLg17cvemAzUrErh4gulvFkvipkG6H4',
+            contentType: "application/json",
+            dataType: 'json',
+            success: function(result){
+                $('#posted').append("<p>" + result.explanation + "</p>")
+            }
+        })
     })
 })
---
+++ algo/altUpperCase.js
@@ -0,0 +1,11 @@
+function capitalize(s){
+    let split_str = s.split('')
+    
+    for (let i = 0; i < s.length; i++) {
+        if (i % 2 === 0) {
+            console.log(split_str[i].toUpperCase())
+        } 
+    }
+}
+
+capitalize("abcdef")
